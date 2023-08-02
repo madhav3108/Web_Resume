64c160f2fd133a456519036ce6a03fa06a3e9015
Madhav Singh <88900164+madhav3108@users.noreply.github.com>
2023-08-02 09:51:53 +0530

    First Commit

---
+++ assets/javascripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#main-nav a").click(function() {
+        $("section").removeClass("show");
+        var target = $(this).attr("href");
+        $(target).addClass("show");
+    });
+
+    $(".type-effect").typed({
+        strings: [" Software Developer , Frontend Developer" , "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+});
